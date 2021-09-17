87516780bd5c3b15c0c905d231c91627de9b9f02
mateusmb <mateusmobueno@gmail.com>
2021-09-16 23:00:49 -0300

    styled components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,9 +4,16 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
     expect(
       screen.getByRole('heading', { name: /boilerplate/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
